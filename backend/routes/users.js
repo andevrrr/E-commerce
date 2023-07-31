911cca5165055adfb6e9a760a5e0f2bc6a826828
Anton <kucherenko-anton1@mail.ru>
2023-07-31 11:44:49 +0300

    /login post

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,13 +4,24 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
         res.status(500).json({success: false})
     } 
     res.send(userList);
 })
+
+router.get("/:id", async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+  
+    if (!user) {
+      res
+        .status(500)
+        .json({ message: "The user with the given ID was not found." });
+    }
+    res.status(200).send(user);
+  });
 
 router.post("/", async (req, res) => {
     let user = new User({
@@ -33,5 +44,15 @@ router.post("/", async (req, res) => {
   
     res.send(user);
   });
+
+router.post('/login', async(req, res) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user) {
+        return res.status(400).send('The user not found')
+    }
+
+    return res.status(200).send(user);
+})
 
 module.exports =router;
